e59bc0dfcb0e8885c2c736d00fb8538f5b1d648c
Stef Stachow <stef@stachow.co.uk>
2021-11-12 14:58:06 +0000

    #8994 - added msal auth, fix issue with msal/crypto in jest

--- rumpole-ui/src/setupTests.ts
+++ rumpole-ui/src/setupTests.ts
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+// prevent issue in https://github.com/AzureAD/microsoft-authentication-library-for-js/issues/1840
+global.crypto = require("crypto");
