bef18fa17518487728059faffe76b90d8a9c9d87
Gopi <gopi_krishnag@hotmail.com>
2021-12-13 16:12:26 +0000

    Comment the code

--- rumpole-gateway/Domain/Authorisation/AzureAdConfig.cs
+++ rumpole-gateway/Domain/Authorisation/AzureAdConfig.cs
@@ -1,15 +1,15 @@
-﻿using System.Collections.Generic;
+﻿//using System.Collections.Generic;
 
-namespace RumpoleGateway.Domain.Authorisation
-{
-    public class AzureAdConfig
-    {
-        public string ClientId { get; set; }
+//namespace RumpoleGateway.Domain.Authorisation
+//{
+//    public class AzureAdConfig
+//    {
+//        public string ClientId { get; set; }
 
-        public string ClientSecret { get; set; }
+//        public string ClientSecret { get; set; }
 
-        public string Authority { get; set; }
+//        public string Authority { get; set; }
 
-        public List<string> Scopes { get; set; }
-    }
-}
+//        public List<string> Scopes { get; set; }
+//    }
+//}
