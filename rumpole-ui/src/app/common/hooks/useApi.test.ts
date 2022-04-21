a22baaee3c54b9b1fb8d30385a7815802111fe0c
Stefan Stachow (ADMIN) <a.sstachow@CPSGOVUK.onmicrosoft.com>
2022-04-21 11:01:44 +0000

    Merged PR 293: #13704 - remove redux from package.json etc; better useApi unit tests
    #13704 - remove redux from package.json etc; better useApi unit tests
    
    Related work items: #13704

--- rumpole-ui/src/app/common/hooks/useApi.test.ts
+++ rumpole-ui/src/app/common/hooks/useApi.test.ts
@@ -1,12 +1,15 @@
 import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/extend-expect";
-import { renderHook } from "@testing-library/react-hooks";
+import { act, renderHook } from "@testing-library/react-hooks";
 import { useApi } from "./useApi";
 
 describe("useApi", () => {
   it("can initiate a call, set status to loading, then return a successful result", async () => {
-    const mockApiCall = (id: string) =>
-      new Promise((resolve) => resolve({ id }));
+    const mockResult = { id: 1 };
+    const mockApiCall = jest.fn(
+      (id: string) =>
+        new Promise((resolve) => setTimeout(() => resolve(mockResult), 10))
+    );
 
     const { result, waitForNextUpdate } = renderHook(() =>
       useApi(mockApiCall, "1")
@@ -14,15 +17,15 @@ describe("useApi", () => {
 
     expect(result.current).toEqual({ status: "loading" });
     await waitForNextUpdate();
-
-    expect(result.current).toEqual({ status: "succeeded", data: { id: "1" } });
+    expect(result.current).toEqual({ status: "succeeded", data: mockResult });
   });
 
   it("can initiate a call, set status to loading, then return an error result", async () => {
-    const apiError = new Error();
-
-    const mockApiCall = (id: string) =>
-      new Promise((_, reject) => reject(apiError));
+    const mockError = new Error();
+    const mockApiCall = jest.fn(
+      (id: string) =>
+        new Promise((_, reject) => setTimeout(() => reject(mockError)))
+    );
 
     const { result, waitForNextUpdate } = renderHook(() =>
       useApi(mockApiCall, "1")
@@ -30,23 +33,63 @@ describe("useApi", () => {
 
     expect(result.current).toEqual({ status: "loading" });
     await waitForNextUpdate();
-
-    expect(result.current).toEqual({ status: "failed", error: apiError });
+    expect(result.current).toEqual({ status: "failed", error: mockError });
   });
 
   it("can initiate a call with multiple parameters", async () => {
-    const mockApiCall = (id: string, otherValue: number, thirdValue: string) =>
-      new Promise((resolve) => resolve({ id, otherValue, thirdValue }));
+    const mockResult = { id: 1 };
+    const mockApiCall = jest.fn(
+      (p1: string, p2: number, p3: string) =>
+        new Promise((resolve) => setTimeout(() => resolve(mockResult), 10))
+    );
 
     const { result, waitForNextUpdate } = renderHook(() =>
       useApi(mockApiCall, "1", 2, "3")
     );
 
+    expect(result.current).toEqual({ status: "loading" });
     await waitForNextUpdate();
-
     expect(result.current).toEqual({
       status: "succeeded",
-      data: { id: "1", otherValue: 2, thirdValue: "3" },
+      data: mockResult,
     });
+  });
+
+  it("can not call the api again if parameters do not change", async () => {
+    const mockResult = { id: 1 };
+    const mockApiCall = jest.fn(
+      (p1: string) =>
+        new Promise((resolve) => setTimeout(() => resolve(mockResult), 10))
+    );
+
+    const { rerender } = renderHook(() => useApi(mockApiCall, "1"));
+
+    expect(mockApiCall.mock.calls.length).toEqual(1);
+    act(() => rerender());
+    expect(mockApiCall.mock.calls.length).toEqual(1);
+  });
+
+  it("can call the api a second time if parameters do change", async () => {
+    const mockResult = { id: 1 };
+    const mockApiCall = jest.fn(
+      (p1: string) =>
+        new Promise((resolve) => setTimeout(() => resolve(mockResult), 10))
+    );
+
+    const { rerender } = renderHook(({ del, p1 }) => useApi(del, p1), {
+      initialProps: {
+        del: mockApiCall,
+        p1: "1",
+      },
+    });
+
+    expect(mockApiCall.mock.calls.length).toEqual(1);
+    act(() => {
+      rerender({
+        del: mockApiCall,
+        p1: "2",
+      });
+    });
+    expect(mockApiCall.mock.calls.length).toEqual(2);
   });
 });
--- rumpole-ui/src/app/common/hooks/useApi.ts
+++ rumpole-ui/src/app/common/hooks/useApi.ts
@@ -13,15 +13,13 @@ type Result<T> =
       status: "loading";
     };
 
-type AllowedPrimitive = string | number | boolean | null | undefined;
-
 type UseApiParams = <T extends (...args: any[]) => Promise<any>>(
   del: T,
-  param0?: Parameters<T>[0] extends AllowedPrimitive ? AllowedPrimitive : never,
-  param1?: Parameters<T>[1] extends AllowedPrimitive ? AllowedPrimitive : never,
-  param2?: Parameters<T>[2] extends AllowedPrimitive ? AllowedPrimitive : never,
-  param3?: Parameters<T>[3] extends AllowedPrimitive ? AllowedPrimitive : never,
-  param4?: Parameters<T>[4] extends AllowedPrimitive ? AllowedPrimitive : never
+  param0?: Parameters<T>[0],
+  param1?: Parameters<T>[1],
+  param2?: Parameters<T>[2],
+  param3?: Parameters<T>[3],
+  param4?: Parameters<T>[4]
 ) => Result<Awaited<ReturnType<typeof del>>>;
 
 /*
@@ -31,14 +29,12 @@ type UseApiParams = <T extends (...args: any[]) => Promise<any>>(
   The `UseApiParams` type ensures that the second (third.. etc) paramters passed to useApi are
   strongly-typed to the argument types of the function passed as the first param e.g. `getFoo`.
 
-  This approach is borrrowed from redux-sagas and avoids the use of anonymous lambdas being passed e.g.
+  This approach is borrowed from redux-sagas and avoids the use of anonymous lambdas being passed e.g.
 
     `const state = useApi(() => getFoo(1, "bar"))`
 
   meaning that on the inside of `useApi` the function is always seen as being different on every execution
-  leading to constant refiring of the `useEffect`.  The use of `AllowedPrimitive` also helps to prevent
-  object (compared by reference) being passed and be seen to differ every execution also, safer to just pass 
-  primitives.
+  leading to constant refiring of the `useEffect`.
 */
 export const useApi: UseApiParams = (del, p0, p1, p2, p3, p4) => {
   const [result, setResult] = useState<ReturnType<UseApiParams>>({
