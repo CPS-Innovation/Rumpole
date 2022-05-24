dfdc8e03e5a88716b666ac7c1b39382c45cd2740
Stefan Stachow (ADMIN) <a.sstachow@CPSGOVUK.onmicrosoft.com>
2022-05-24 12:41:19 +0000

    Merged PR 338: #13939 - add delay to dev pdf pipeline
    #13939 - add delay to dev pdf pipeline
    
    Related work items: #13939

--- rumpole-ui/src/mock-api/data/pipelinePdfResults.dev.ts
+++ rumpole-ui/src/mock-api/data/pipelinePdfResults.dev.ts
@@ -1,7 +1,30 @@
+import { PdfDocument } from "../../app/features/cases/domain/PdfDocument";
 import { PipelineResults } from "../../app/features/cases/domain/PipelineResults";
 import { PipelinePdfResultsDataSource } from "./types/PipelinePdfResultsDataSource";
 
-const dataSource: PipelinePdfResultsDataSource = () => pipelinePdfResults;
+let callIndex = 0;
+const dataSource: PipelinePdfResultsDataSource = () => {
+  if (callIndex > pipelinePdfResults.documents.length) {
+    callIndex = 0;
+  }
+
+  console.log(callIndex);
+  const incrementingData = pipelinePdfResults.documents.reduce(
+    (prev, curr, index) => {
+      if (index >= callIndex) {
+        // whilst callIndex grows, any documents at positions greater or equal
+        // to callIndex will return a not ready record (empty pdfBlobName)
+        return [...prev, { ...curr, pdfBlobName: "" }];
+      } else {
+        return [...prev, curr];
+      }
+    },
+    [] as PdfDocument[]
+  );
+
+  callIndex += 1;
+  return { ...pipelinePdfResults, documents: incrementingData };
+};
 
 export default dataSource;
 
@@ -10,7 +33,27 @@ const pipelinePdfResults: PipelineResults = {
   documents: [
     {
       documentId: "d1",
+      pdfBlobName: "MCLOVEMG3",
+      status: "finished",
+    },
+    {
+      documentId: "d2",
       pdfBlobName: "CM01",
+      status: "finished",
+    },
+    {
+      documentId: "d3",
+      pdfBlobName: "MG05MCLOVE",
+      status: "finished",
+    },
+    {
+      documentId: "d4",
+      pdfBlobName: "MG06_3June",
+      status: "finished",
+    },
+    {
+      documentId: "d5",
+      pdfBlobName: "MG06_10june",
       status: "finished",
     },
   ],
