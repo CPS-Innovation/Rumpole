f8647bddabf3c8b4e4b4b9394c7e2fd8b971dc36
Mark Jones <Mark.Jones3@cps.gov.uk>
2022-08-10 10:06:59 +0000

    Merged PR 429: Improved client stub logging

--- rumpole-gateway/Clients/DocumentRedaction/DocumentRedactionClientStub.cs
+++ rumpole-gateway/Clients/DocumentRedaction/DocumentRedactionClientStub.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Threading.Tasks;
 using Aspose.Pdf;
 using Aspose.Pdf.Annotations;
+using Microsoft.Extensions.Logging;
 using RumpoleGateway.Clients.RumpolePipeline;
 using RumpoleGateway.Domain.DocumentRedaction;
 using RumpoleGateway.Services;
@@ -13,11 +14,13 @@ namespace RumpoleGateway.Clients.DocumentRedaction
     {
         private readonly IBlobStorageClient _blobStorageClient;
         private readonly ISasGeneratorService _sasGeneratorService;
+        private readonly ILogger<DocumentRedactionClientStub> _logger;
 
-        public DocumentRedactionClientStub(IBlobStorageClient blobStorageClient, ISasGeneratorService sasGeneratorService)
+        public DocumentRedactionClientStub(IBlobStorageClient blobStorageClient, ISasGeneratorService sasGeneratorService, ILogger<DocumentRedactionClientStub> logger)
         {
             _blobStorageClient = blobStorageClient;
             _sasGeneratorService = sasGeneratorService;
+            _logger = logger;
         }
 
         public Task<DocumentRedactionStatus> CheckOutDocument(string caseId, string documentId, string accessToken)
@@ -34,47 +37,59 @@ namespace RumpoleGateway.Clients.DocumentRedaction
         {
             var saveResult = new DocumentRedactionSaveResult();
 
-            var document = await _blobStorageClient.GetDocumentAsync(fileName);
-            if (document == null)
+            try
             {
-                saveResult.Succeeded = false;
-                saveResult.Message = $"Invalid document - a document with filename '{fileName}' could not be retrieved for redaction purposes";
+                var document = await _blobStorageClient.GetDocumentAsync(fileName);
+                if (document == null)
+                {
+                    saveResult.Succeeded = false;
+                    saveResult.Message = $"Invalid document - a document with filename '{fileName}' could not be retrieved for redaction purposes";
+                    return saveResult;
+                }
+
+                var fileNameWithoutExtension = fileName.IndexOf(".pdf", StringComparison.OrdinalIgnoreCase) > -1 ? fileName.Split(".pdf", StringSplitOptions.RemoveEmptyEntries)[0] : fileName;
+                var newFileName = $"{fileNameWithoutExtension}_{DateTime.Now.Ticks.GetHashCode().ToString("x").ToUpper()}.pdf";
+
+                using var doc = new Document(document);
+
+                foreach (var redactionPage in saveRequest.Redactions)
+                {
+                    var currentPage = redactionPage.PageIndex + 1;
+                    var annotationPage = doc.Pages[currentPage];
+                    foreach (var boxToDraw in redactionPage.RedactionCoordinates)
+                    {
+                        var annotationRectangle = new Rectangle(boxToDraw.X1, boxToDraw.Y1, boxToDraw.X2, boxToDraw.Y2);
+                        var redactionAnnotation = new RedactionAnnotation(annotationPage, annotationRectangle)
+                            {
+                                FillColor = Color.Black
+                            };
+
+                        doc.Pages[currentPage].Annotations.Add(redactionAnnotation, true);
+                        redactionAnnotation.Redact();
+                    }
+                }
+
+                using var redactedDocumentStream = new MemoryStream();
+                doc.Save(redactedDocumentStream);
+
+                await _blobStorageClient.UploadDocumentAsync(redactedDocumentStream, newFileName);
+
+                var redactedFileUri = await _sasGeneratorService.GenerateSasUrlAsync(newFileName);
+
+                saveResult.Succeeded = true;
+                saveResult.RedactedDocumentUrl = redactedFileUri;
+
                 return saveResult;
             }
-
-            var fileNameWithoutExtension = fileName.IndexOf(".pdf", StringComparison.OrdinalIgnoreCase) > -1 ? fileName.Split(".pdf", StringSplitOptions.RemoveEmptyEntries)[0] : fileName;
-            var newFileName = $"{fileNameWithoutExtension}_{DateTime.Now.Ticks.GetHashCode().ToString("x").ToUpper()}.pdf";
-
-            using var doc = new Document(document);
-
-            foreach (var redactionPage in saveRequest.Redactions)
+            catch (Exception ex)
             {
-                var currentPage = redactionPage.PageIndex + 1;
-                var annotationPage = doc.Pages[currentPage];
-                foreach (var boxToDraw in redactionPage.RedactionCoordinates)
-                {
-                    var annotationRectangle = new Rectangle(boxToDraw.X1, boxToDraw.Y1, boxToDraw.X2, boxToDraw.Y2);
-                    var redactionAnnotation = new RedactionAnnotation(annotationPage, annotationRectangle)
-                        {
-                            FillColor = Color.Black
-                        };
+                var message = $"Could not complete the redaction request for filename '{fileName}'";
+                _logger.LogError(ex, message);
 
-                    doc.Pages[currentPage].Annotations.Add(redactionAnnotation, true);
-                    redactionAnnotation.Redact();
-                }
+                saveResult.Succeeded = false;
+                saveResult.Message = $"{message} - {ex.Message}";
+                return saveResult;
             }
-
-            using var redactedDocumentStream = new MemoryStream();
-            doc.Save(redactedDocumentStream);
-
-            await _blobStorageClient.UploadDocumentAsync(redactedDocumentStream, newFileName);
-
-            var redactedFileUri = await _sasGeneratorService.GenerateSasUrlAsync(newFileName);
-
-            saveResult.Succeeded = true;
-            saveResult.RedactedDocumentUrl = redactedFileUri;
-
-            return saveResult;
         }
     }
 }
