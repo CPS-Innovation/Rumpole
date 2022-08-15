b8e6ed793bc4626a4e8b2ab61752645af3ffa5aa
Mark Jones <aceltuk@users.noreply.github.com>
2022-08-15 16:37:52 +0100

    Feature/converting pipelines to actions (#10)
    * Initial reformatting of devops-configured YAML into github actions, with issues related to missing components - will resolve these using GitHub's editor
    
    * Added code coverage action, replacing incompatible devops definition
    
    * Amended location searched for code coverage xml
    
    * Renamed UI pipeline
    
    * Tweaking code coverage result format
    
    * Altererd workflow for manual dispatch for testing purposes
    
    * Another attempt at the reconfigured actions definition
    
    * Further amendments
    
    * Reworked test invocation
    
    * test
    
    * d
    
    * Added location of test results
    
    * Tweaked location again and removed unecessary file copy
    
    * One more
    
    * Changing coverage reporting to coveralls.io
    
    * Back to coverlet
    
    * Removed coverlet from non-test projects because it was skewing the coverage results
    
    * Corrected filename for codecoverage summary
    
    * Covering CoreDataApi client with tests

--- rumpole-gateway.tests/Clients/CoreDataApi/CoreDataApiClientTests.cs
+++ rumpole-gateway.tests/Clients/CoreDataApi/CoreDataApiClientTests.cs
@@ -1,69 +1,217 @@
-﻿//using System.Threading.Tasks;
-//using GraphQL;
-//using GraphQL.Client.Abstractions;
-//using GraphQL.Client.Http;
-//using Microsoft.Extensions.Logging;
-//using Moq;
-//using NSubstitute;
-//using RumpoleGateway.Clients.CoreDataApi;
-//using RumpoleGateway.Domain.CoreDataApi.ResponseTypes;
-//using RumpoleGateway.Extensions;
-//using RumpoleGateway.Factories.AuthenticatedGraphQLHttpRequestFactory;
-//using RumpoleGateway.Tests.FakeData;
-//using Xunit;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
+using GraphQL;
+using GraphQL.Client.Abstractions;
+using GraphQL.Client.Http;
+using Moq;
+using RumpoleGateway.Clients.CoreDataApi;
+using RumpoleGateway.Domain.CoreDataApi.CaseDetails;
+using RumpoleGateway.Domain.CoreDataApi.ResponseTypes;
+using RumpoleGateway.Extensions;
+using RumpoleGateway.Factories.AuthenticatedGraphQLHttpRequestFactory;
+using RumpoleGateway.Tests.FakeData;
+using Xunit;
 
-//namespace RumpoleGateway.Tests.Clients.CoreDataApi
-//{
-//    public class CoreDataApiClientTests : IClassFixture<ResponseCaseDetailsFake>
-//    {
-//        private readonly Mock<IGraphQLClient> _coreDataApiClientMock;
-//        private readonly Mock<IAuthenticatedGraphQLHttpRequestFactory> _authenticatedGraphQLHttpRequestFactoryMock;
-//        private readonly Mock<ILogger<CoreDataApiClient>> _loggerMock;
-//        private readonly ResponseCaseDetailsFake _responseCaseDetailsFake;
+namespace RumpoleGateway.Tests.Clients.CoreDataApi
+{
+    public class CoreDataApiClientTests : IClassFixture<ResponseCaseDetailsFake>
+    {
+        private readonly Mock<IGraphQLClient> _coreDataApiClientMock;
+        private readonly Mock<IAuthenticatedGraphQLHttpRequestFactory> _authenticatedGraphQlHttpRequestFactoryMock;
+        private readonly ResponseCaseDetailsFake _responseCaseDetailsFake;
+        private readonly Fixture _fixture;
 
-//        public CoreDataApiClientTests()
-//        {
-//            _coreDataApiClientMock = new Mock<IGraphQLClient>();
-//            _authenticatedGraphQLHttpRequestFactoryMock = new Mock<IAuthenticatedGraphQLHttpRequestFactory>();
-//            _loggerMock = new Mock<ILogger<CoreDataApiClient>>();
-//        }
-//        public CoreDataApiClientTests(ResponseCaseDetailsFake responseCaseDetailsFake)
-//        {
-//            _responseCaseDetailsFake = responseCaseDetailsFake;
-//        }
+        public CoreDataApiClientTests()
+        {
+            _coreDataApiClientMock = new Mock<IGraphQLClient>();
+            _authenticatedGraphQlHttpRequestFactoryMock = new Mock<IAuthenticatedGraphQLHttpRequestFactory>();
+            _fixture = new Fixture();
+            _responseCaseDetailsFake = new ResponseCaseDetailsFake();
+        }
+        
+        [Fact]
+        public async Task CoreDataApiClient_GetCaseDetailsById_Should_Return_Response_Valid_response()
+        {
+            //Arrange
+            _authenticatedGraphQlHttpRequestFactoryMock.Setup(x => x.Create(It.IsAny<string>(), It.IsAny<GraphQLHttpRequest>()))
+                                                                    .Returns(new AuthenticatedGraphQLHttpRequest(_fixture.Create<string>(), _fixture.Create<GraphQLHttpRequest>()));
 
-//        [Fact]
-//        public async Task CoreDataApiClient_GetCaseDetailsById_Should_Return_Response_Valid_response()
-//        {
-//            //Arrange
-//            _authenticatedGraphQLHttpRequestFactoryMock.Setup(x => x.Create(It.IsAny<string>(), It.IsAny<GraphQLHttpRequest>()))
-//                                                                    .Returns(new AuthenticatedGraphQLHttpRequest(It.IsAny<string>(), It.IsAny<GraphQLHttpRequest>()));
-
+            var fakedResponse = new GraphQLResponse<ResponseCaseDetails>
+            {
+                Data = _responseCaseDetailsFake.GetCaseDetailsResponse_Payload()
+            };
+            _coreDataApiClientMock.Setup(x => x.SendQueryAsync<ResponseCaseDetails>(It.IsAny<GraphQLRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(fakedResponse);
             
-//            _coreDataApiClientMock.Setup(x => x.SendQueryAsync(request,default)
-//                                        .ReturnsAsync(_responseCaseDetailsFake.GetCaseDetailsResponse_Payload()));
-//            //_coreDataApiClientMock.SendQueryAsync<ResponseCaseDetails>(null)
-//            //    .Returns(Task.FromResult(GraphQL.GraphQLResponse<_responseCaseDetailsFake.GetCaseDetailsResponse_Payload()>));
-            
-//            var coreDataApiClient = GetCoreDataApiClient();
+            var coreDataApiClient = GetCoreDataApiClient();
 
-//            //Act
-//            var results = await coreDataApiClient.GetCaseDetailsById(It.IsAny<string>(), It.IsAny<string>());
+            //Act
+            var results = await coreDataApiClient.GetCaseDetailsByIdAsync(_fixture.Create<string>(), _fixture.Create<string>());
 
-//            //Assert
-//            //Assert.Equal(401, results.StatusCode);
-//            //Assert.Equal(Constants.CommonUserMessages.AuthenticationFailedMessage, results.Value);
-//        }
+            //Assert
+            results.Id.Should().Be(fakedResponse.Data.CaseDetails.Id);
+        }
 
-//        #region private methods
-//        //private CoreDataApiClient GetCoreDataApiClient()
-//        //{
-//        //    return new CoreDataApiClient(_coreDataApiClientMock,
-//        //                                 _authenticatedGraphQLHttpRequestFactoryMock,
-//        //                                 _loggerMock)
-//        //    { };
-//        //}
-//        #endregion private methods
-//    }
+        [Fact]
+        public async Task CoreDataApiClient_GetCaseDetailsById_WhenResponseData_IsNull_ReturnsNull()
+        {
+            //Arrange
+            _authenticatedGraphQlHttpRequestFactoryMock.Setup(x => x.Create(It.IsAny<string>(), It.IsAny<GraphQLHttpRequest>()))
+                .Returns(new AuthenticatedGraphQLHttpRequest(_fixture.Create<string>(), _fixture.Create<GraphQLHttpRequest>()));
 
-//}
+            var fakedResponse = new GraphQLResponse<ResponseCaseDetails>
+            {
+                Data = null
+            };
+            _coreDataApiClientMock.Setup(x => x.SendQueryAsync<ResponseCaseDetails>(It.IsAny<GraphQLRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(fakedResponse);
+
+            var coreDataApiClient = GetCoreDataApiClient();
+
+            //Act
+            var results = await coreDataApiClient.GetCaseDetailsByIdAsync(_fixture.Create<string>(), _fixture.Create<string>());
+
+            //Assert
+            results.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task CoreDataApiClient_GetCaseDetailsById_WhenCaseDetails_IsNull_ReturnsNull()
+        {
+            //Arrange
+            _authenticatedGraphQlHttpRequestFactoryMock.Setup(x => x.Create(It.IsAny<string>(), It.IsAny<GraphQLHttpRequest>()))
+                .Returns(new AuthenticatedGraphQLHttpRequest(_fixture.Create<string>(), _fixture.Create<GraphQLHttpRequest>()));
+
+            var fakedResponse = new GraphQLResponse<ResponseCaseDetails>
+            {
+                Data = _responseCaseDetailsFake.GetCaseDetailsResponse_Payload()
+            };
+            fakedResponse.Data.CaseDetails = null;
+            _coreDataApiClientMock.Setup(x => x.SendQueryAsync<ResponseCaseDetails>(It.IsAny<GraphQLRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(fakedResponse);
+
+            var coreDataApiClient = GetCoreDataApiClient();
+
+            //Act
+            var results = await coreDataApiClient.GetCaseDetailsByIdAsync(_fixture.Create<string>(), _fixture.Create<string>());
+
+            //Assert
+            results.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task CoreDataApiClient_GetCaseDetailsById_WhenCaseDetails_ThrowsException_IsCaughtSuccessfully()
+        {
+            //Arrange
+            _authenticatedGraphQlHttpRequestFactoryMock.Setup(x => x.Create(It.IsAny<string>(), It.IsAny<GraphQLHttpRequest>()))
+                .Returns(new AuthenticatedGraphQLHttpRequest(_fixture.Create<string>(), _fixture.Create<GraphQLHttpRequest>()));
+
+            var fakedResponse = new GraphQLResponse<ResponseCaseDetails>
+            {
+                Data = _responseCaseDetailsFake.GetCaseDetailsResponse_Payload()
+            };
+            fakedResponse.Data.CaseDetails = null;
+            _coreDataApiClientMock.Setup(x => x.SendQueryAsync<ResponseCaseDetails>(It.IsAny<GraphQLRequest>(), It.IsAny<CancellationToken>()))
+                .Throws<Exception>();
+
+            var coreDataApiClient = GetCoreDataApiClient();
+
+            //Act
+            var results = async () => await coreDataApiClient.GetCaseDetailsByIdAsync(_fixture.Create<string>(), _fixture.Create<string>());
+
+            //Assert
+            await results.Should().ThrowAsync<Exception>();
+        }
+
+        [Fact]
+        public async Task CoreDataApiClient_GetCaseInformationByUrnAsync_Should_Return_Response_Valid_response()
+        {
+            //Arrange
+            _authenticatedGraphQlHttpRequestFactoryMock.Setup(x => x.Create(It.IsAny<string>(), It.IsAny<GraphQLHttpRequest>()))
+                                                                    .Returns(new AuthenticatedGraphQLHttpRequest(_fixture.Create<string>(), _fixture.Create<GraphQLHttpRequest>()));
+
+            var fakedResponse = _fixture.Create<GraphQLResponse<ResponseCaseInformationByUrn>>();
+            fakedResponse.Data.CaseDetails = _fixture.CreateMany<CaseDetails>(5).ToList();
+            _coreDataApiClientMock.Setup(x => x.SendQueryAsync<ResponseCaseInformationByUrn>(It.IsAny<GraphQLRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(fakedResponse);
+
+            var coreDataApiClient = GetCoreDataApiClient();
+
+            //Act
+            var results = await coreDataApiClient.GetCaseInformationByUrnAsync(_fixture.Create<string>(), _fixture.Create<string>());
+
+            //Assert
+            results.Count.Should().Be(5);
+        }
+
+        [Fact]
+        public async Task CoreDataApiClient_GetCaseInformationByUrnAsync_WhenResponseData_IsNull_ReturnsNull()
+        {
+            //Arrange
+            _authenticatedGraphQlHttpRequestFactoryMock.Setup(x => x.Create(It.IsAny<string>(), It.IsAny<GraphQLHttpRequest>()))
+                .Returns(new AuthenticatedGraphQLHttpRequest(_fixture.Create<string>(), _fixture.Create<GraphQLHttpRequest>()));
+
+            var fakedResponse = _fixture.Create<GraphQLResponse<ResponseCaseInformationByUrn>>();
+            fakedResponse.Data = null;
+            _coreDataApiClientMock.Setup(x => x.SendQueryAsync<ResponseCaseInformationByUrn>(It.IsAny<GraphQLRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(fakedResponse);
+
+            var coreDataApiClient = GetCoreDataApiClient();
+
+            //Act
+            var results = await coreDataApiClient.GetCaseInformationByUrnAsync(_fixture.Create<string>(), _fixture.Create<string>());
+
+            //Assert
+            results.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task CoreDataApiClient_GetCaseInformationByUrnAsync_WhenCaseDetails_IsNull_ReturnsNull()
+        {
+            _authenticatedGraphQlHttpRequestFactoryMock.Setup(x => x.Create(It.IsAny<string>(), It.IsAny<GraphQLHttpRequest>()))
+                .Returns(new AuthenticatedGraphQLHttpRequest(_fixture.Create<string>(), _fixture.Create<GraphQLHttpRequest>()));
+
+            var fakedResponse = _fixture.Create<GraphQLResponse<ResponseCaseInformationByUrn>>();
+            fakedResponse.Data.CaseDetails = null;
+            _coreDataApiClientMock.Setup(x => x.SendQueryAsync<ResponseCaseInformationByUrn>(It.IsAny<GraphQLRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(fakedResponse);
+
+            var coreDataApiClient = GetCoreDataApiClient();
+
+            //Act
+            var results = await coreDataApiClient.GetCaseInformationByUrnAsync(_fixture.Create<string>(), _fixture.Create<string>());
+
+            //Assert
+            results.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task CoreDataApiClient_GetCaseInformationByUrnAsync_ThrowsException_IsCaughtSuccessfully()
+        {
+            //Arrange
+            _authenticatedGraphQlHttpRequestFactoryMock.Setup(x => x.Create(It.IsAny<string>(), It.IsAny<GraphQLHttpRequest>()))
+                .Returns(new AuthenticatedGraphQLHttpRequest(_fixture.Create<string>(), _fixture.Create<GraphQLHttpRequest>()));
+
+            var fakedResponse = _fixture.Create<GraphQLResponse<ResponseCaseInformationByUrn>>();
+            fakedResponse.Data.CaseDetails = _fixture.CreateMany<CaseDetails>(5).ToList();
+            _coreDataApiClientMock.Setup(x => x.SendQueryAsync<ResponseCaseInformationByUrn>(It.IsAny<GraphQLRequest>(), It.IsAny<CancellationToken>()))
+                .Throws<Exception>();
+
+            var coreDataApiClient = GetCoreDataApiClient();
+
+            //Act
+            var results = async () => await coreDataApiClient.GetCaseInformationByUrnAsync(_fixture.Create<string>(), _fixture.Create<string>());
+
+            //Assert
+            await results.Should().ThrowAsync<Exception>();
+        }
+
+        #region private methods
+
+        private CoreDataApiClient GetCoreDataApiClient()
+        {
+            return new CoreDataApiClient(_coreDataApiClientMock.Object, _authenticatedGraphQlHttpRequestFactoryMock.Object);
+        }
+
+        #endregion private methods
+    }
+
+}
