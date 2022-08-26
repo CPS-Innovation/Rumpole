5124be3be32c49ddc2e37cb03a9d422183046b88
Stef Stachow <stef@stachow.co.uk>
2022-08-26 14:49:16 +0100

    #14991 - improve pdf loading wait content

--- rumpole-ui/src/app/features/cases/presentation/case-details/pdf-viewer/Wait.tsx
+++ rumpole-ui/src/app/features/cases/presentation/case-details/pdf-viewer/Wait.tsx
@@ -1,3 +1,11 @@
+import { Spinner } from "../../../../../common/presentation/components/Spinner";
+
+import classes from "./Wait.module.scss";
+
 export const Wait: React.FC = () => {
-  return <span className="govuk-body">Please wait...</span>;
+  return (
+    <span className={classes.content}>
+      <Spinner diameterPx={50} />
+    </span>
+  );
 };
