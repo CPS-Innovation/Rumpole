efa303687aff311c25cae9ebf3238c642819d1bf
aceltuk <mark.jones3@cps.gov.uk>
2022-09-08 15:23:39 +0100

    Have to explicitly set client_secret_setting_name to an empty string when reverting from authv2 to authv1

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -66,9 +66,10 @@ resource "azurerm_function_app" "fa_rumpole" {
     unauthenticated_client_action = "RedirectToLoginPage"
     default_provider              = "AzureActiveDirectory"
     active_directory {
-      client_id         = azuread_application.fa_rumpole.application_id
-      client_secret     = azuread_application_password.faap_rumpole_app_service.value
-      allowed_audiences = ["https://CPSGOVUK.onmicrosoft.com/fa-${local.resource_name}-gateway"]
+      client_id                   = azuread_application.fa_rumpole.application_id
+      client_secret               = azuread_application_password.faap_rumpole_app_service.value
+      client_secret_setting_name  = ""
+      allowed_audiences           = ["https://CPSGOVUK.onmicrosoft.com/fa-${local.resource_name}-gateway"]
     }
   }
 
