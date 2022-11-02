4f76e5d7a775afcd8f6bb3b87097f284ce58d34b
Mark Jones <aceltuk@users.noreply.github.com>
2022-11-02 16:44:20 +0000

    Update functions.tf

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -64,7 +64,7 @@ resource "azurerm_linux_function_app" "fa_rumpole" {
     default_provider              = "AzureActiveDirectory"
     active_directory {
       client_id                  = azuread_application.fa_rumpole.application_id
-      client_secret              = azuread_application_password.faap_rumpole_app_service.value
+      client_secret_setting_name = "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET"
       allowed_audiences          = ["https://CPSGOVUK.onmicrosoft.com/fa-${local.resource_name}-gateway"]
       }
   }
