c1f742fce72dddf30629b4789c63061f4f7d0fbd
aceltuk <mark.jones3@cps.gov.uk>
2022-11-02 11:55:32 +0000

    Upgrading terraform to version 3

--- terraform/app-service-plan.tf
+++ terraform/app-service-plan.tf
@@ -4,7 +4,7 @@ resource "azurerm_app_service_plan" "asp_rumpole" {
   name                = "asp-${local.resource_name}"
   location            = azurerm_resource_group.rg_rumpole.location
   resource_group_name = azurerm_resource_group.rg_rumpole.name
-  kind                = "Linux"
+  os_type             = "Linux"
   reserved            = true
 
   sku {
