a7cfc3f47f22337967f8892b6dd10b4e6537cc64
aceltuk <mark.jones3@cps.gov.uk>
2022-11-10 10:19:01 +0000

    Removed invalid character from resource group definition in lookups.tf

--- terraform/lookups.tf
+++ terraform/lookups.tf
@@ -8,15 +8,15 @@ data "azuread_application" "fa_pipeline_pdf_generator" {
 
 data "azurerm_function_app_host_keys" "fa_pipeline_coordinator_host_keys" {
   name                = "fa-${local.pipeline_resource_name}-coordinator"
-  resource_group_name = "rg=${local.pipeline_resource_name}"
+  resource_group_name = "rg-${local.pipeline_resource_name}"
 }
 
 data "azurerm_function_app_host_keys" "fa_pipeline_pdf_generator_host_keys" {
   name                = "fa-${local.pipeline_resource_name}-pdf-generator"
-  resource_group_name = "rg=${local.pipeline_resource_name}"
+  resource_group_name = "rg-${local.pipeline_resource_name}"
 }
 
 data "azurerm_search_service" "pipeline_ss" {
   name                = "ss-${local.pipeline_resource_name}"
-  resource_group_name = "rg=${local.pipeline_resource_name}"
+  resource_group_name = "rg-${local.pipeline_resource_name}"
 }
