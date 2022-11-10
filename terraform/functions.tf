68587e0aa21dbdefd8e706633eed32f466835b76
aceltuk <mark.jones3@cps.gov.uk>
2022-11-10 11:56:35 +0000

    Added the gateway as a preauthorized app to the pipeline coordinator

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -141,6 +141,12 @@ resource "azuread_application_password" "faap_rumpole_app_service" {
   end_date_relative     = "17520h"
 }
 
+resource "azuread_application_pre_authorized" "fapre_fa_coordinator" {
+  application_object_id = data.azuread_application.fa_pipeline_coordinator.id
+  authorized_app_id     = module.azurerm_app_reg_fa_rumpole.client_id
+  permission_ids        = [data.azuread_application.fa_pipeline_coordinator.oauth2_permission_scope_ids["user_impersonation"]]
+}
+
 resource "azuread_application_pre_authorized" "fapre_fa_pdf-generator" {
   application_object_id = data.azuread_application.fa_pipeline_pdf_generator.id
   authorized_app_id     = module.azurerm_app_reg_fa_rumpole.client_id
