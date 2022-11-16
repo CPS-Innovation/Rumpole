e9435aca3507ff51b81c8fd753f6e2fdf46f2eeb
aceltuk <mark.jones3@cps.gov.uk>
2022-11-16 08:29:54 +0000

    Reverted unsupported (in v1) use of client_secret_setting_name

--- terraform/app-service.tf
+++ terraform/app-service.tf
@@ -34,8 +34,7 @@ resource "azurerm_app_service" "as_web_rumpole" {
     token_store_enabled           = true
     active_directory {
       client_id         = module.azurerm_app_reg_as_web_rumpole.client_id
-      //client_secret     = azuread_application_password.asap_web_rumpole_app_service.value
-      client_secret_setting_name = ""
+      client_secret     = azuread_application_password.asap_web_rumpole_app_service.value
       allowed_audiences = ["https://CPSGOVUK.onmicrosoft.com/as-web-${local.resource_name}"]
     }
   }
