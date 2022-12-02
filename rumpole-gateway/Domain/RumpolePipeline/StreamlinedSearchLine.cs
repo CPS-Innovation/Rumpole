80b5a20c264f0cebe0bf15a7a40d6a3124e682d9
Mark Jones <aceltuk@users.noreply.github.com>
2022-12-02 12:51:31 +0000

    Update StreamlinedSearchLine.cs
    Bringing the StreamlinedSearchLine up-to-date with the latest model from the Pipeline

--- rumpole-gateway/Domain/RumpolePipeline/StreamlinedSearchLine.cs
+++ rumpole-gateway/Domain/RumpolePipeline/StreamlinedSearchLine.cs
@@ -9,22 +9,28 @@ namespace RumpoleGateway.Domain.RumpolePipeline
         public string Id { get; set; }
 
         [JsonProperty("caseId")]
-        public int CaseId { get; set; }
+        public long CaseId { get; set; }
 
         [JsonProperty("documentId")]
         public string DocumentId { get; set; }
+    
+        [JsonProperty("versionId")]
+        public long VersionId { get; set; }
 
+        [JsonProperty("fileName")]
+        public string FileName { get; set; }
+    
         [JsonProperty("pageIndex")]
         public int PageIndex { get; set; }
+
+        [JsonProperty("lineIndex")]
+        public int LineIndex { get; set; }
 
         [JsonProperty("pageHeight")]
         public double PageHeight { get; set; }
 
         [JsonProperty("pageWidth")]
         public double PageWidth { get; set; }
-
-        [JsonProperty("lineIndex")]
-        public int LineIndex { get; set; }
 
         [JsonProperty(PropertyName = "text")]
         public string Text { get; set; }
